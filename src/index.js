16df2573a43a7f35dd1ef70ae6f25230516df1ce
Ediful <56943939+Ediful@users.noreply.github.com>
2022-02-21 21:16:38 -0600

    :feat highlight for ship placement while hovering

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import player from './player';
 import gameboard from './gameboard';
 import ui from './ui';
 
-window.addEventListener('load', () => {
-  const player1 = player();
-  const playerBoard = gameboard();
+const player1 = player();
+const playerBoard = gameboard();
 
-  const enemy = player();
-  const enemyBoard = gameboard();
+const enemy = player();
+const enemyBoard = gameboard();
 
-  document.querySelector('h2').textContent = 'Place your ships';
+document.querySelector('h2').textContent = 'Place your ships';
 
-  const disa = ui();
-  disa.placeShip(5);
-});
+const disa = ui();
+disa.placeShip(5);
--- src/ui.js
+++ src/ui.js
@@ -1,21 +1,50 @@
 export default (() => {
-  const placeShip = () => {
+  const placeShipHighlight = () => {
     const boardItem = document.getElementsByClassName('board-item');
 
     Array.from(boardItem).forEach((item) => {
-      item.addEventListener('mouseover', () => {
+      item.addEventListener('mouseenter', () => {
         const itemPos = Array.from(item.parentNode.children).indexOf(item);
-        // boardItem[itemPos].classList.toggle('ship-highlight:hover');
         for (let i = 0; i < 5; i += 1) {
-          // boardItem[itemPos + 1].style.backgroundColor = 'red';
-          boardItem[itemPos + 1].classList.add('ship-highlight');
+          boardItem[itemPos + i].classList.add('ship-highlight');
+        }
+      });
+    });
+
+    Array.from(boardItem).forEach((item) => {
+      item.addEventListener('mouseout', () => {
+        const itemPos = Array.from(item.parentNode.children).indexOf(item);
+        for (let i = 0; i < 5; i += 1) {
+          boardItem[itemPos + i].classList.remove('ship-highlight');
         }
       });
     });
   };
 
-  const placeShipHighlight = () => {
+  const placeShip = () => {
+    const boardItem = document.getElementsByClassName('board-item');
 
+    placeShipHighlight();
+
+    Array.from(boardItem).forEach((item) => {
+      item.addEventListener('click', () => {
+        const itemPos = Array.from(item.parentNode.children).indexOf(item);
+        console.log(itemPos);
+      });
+    });
+  };
+
+  const boardItemEvent = (event) => {
+    const boardItem = document.getElementsByClassName('board-item');
+
+    Array.from(boardItem).forEach((item) => {
+      item.addEventListener('mouseout', () => {
+        const itemPos = Array.from(item.parentNode.children).indexOf(item);
+        for (let i = 0; i < 5; i += 1) {
+          boardItem[itemPos + i].classList.remove('ship-highlight');
+        }
+      });
+    });
   };
 
   const drawShips = () => {
